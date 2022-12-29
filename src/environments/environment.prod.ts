dd5fe7c13a7929be47886a59812c5682279a9b34
soporte2@xerintel.es <fXQ81%u6>
2022-12-29 14:36:44 +0100

    manifest

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   domainUrl: 'https://development.xerintel.net/',
 
   //URL del endpoint de la api de pruebas
-  apiUrl: 'https://development.xerintel.net/api/auth/',
+  apiUrl: 'https://192.168.0.114/laravel-8-base/public/api/auth/',
 
   stripePublishableKey:'pk_test_51KyZ1OAcr41a1NJzp6OCyIMMAKAlTd3Z8uBVA127yqHoPIbdysIdRVJInpkUCsgqBQXbWWsQwStjelozXAbEv0bc00JEy5JxVR',
 };
