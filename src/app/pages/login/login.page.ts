934e5cdfe0fcf609864261a8eed38e9b1a3ff7be
Jesus Hernandez Ramirez <jesushr0013@gmail.com>
2023-01-03 07:16:40 +0100

    info, keystore, env

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -82,9 +82,19 @@ export class LoginPage implements OnInit {
   }
 
 
-  public loginApple(){
-
-  }
+  /**
+   * ------------------------------------------- REGISTRO LOGIN GOOGLE CAPACITOR --------------------------------------------------------------------------------------
+   * 1. Crear y/o consutar proyecto en Firebase
+   * 2. Añadir la app para android y/o iOS y seguir los diferentes pasos
+   * 3. Añadir hasta 3 Huellas digitales del certificado SHA (SHA-1) (debug, release y google play console)
+   *    - a) keytool -list -v -keystore PATH_DEBUG_KEYSTORE -alias androiddebugkey
+   *    - b) keytool -list -v -keystore PATH_KEYSTORE -alias ALIAS_NAME
+   *    - c) "Integridad de la aplicación" dentro de la App de Google Play Console
+   * 4. Añadir google a la app: Authentication --> Sign-in method --> Habilitar Google
+   * 5. Descargar el archivo google-services.json (AÑADIR O REEMPLAZAR en la carpeta android > app) y GoogleService-Info.plist (AÑADIR O REEMPLAZAR en carpeta de ios) 
+   * 6. MODIICAR capacitor.config.json con androidClientId (WEB CLIENT ID) y iosClientId (REVERSED CLIENT ID) 
+   * ------------------------------------------------------------------------------------------------------------------------------------------------------------------ 
+  */
 
   public async loginGoogle(){
     const user = await GoogleAuth.signIn();
@@ -92,6 +102,31 @@ export class LoginPage implements OnInit {
     console.log("user" , user);
     
   }
+
+
+
+  /**
+   * ------------------------------------------- REGISTRO LOGIN CON FACEBOOK -------------------------------------------
+   * 
+   * 1. Registrar la nueva app en : https://developers.facebook.com/apps/
+   * 2. Añadir el Login/Registro con Facebook, para Android y/o iOS : 
+   *    - Seguir los diferentes pasos para la creación de de los hash (debug and release) (PARA ANDROID)
+   *    ---- EJEMPLO GENERACIÓN DEL HASH : keytool -exportcert -alias ALIAS_O_ALIAS_DEBUG -keystore "RUTA_DEBUG_KEY_O_RELEASE_KEY" | "RUTA_OPENSSL" sha1 -binary | "RUTA_OPENSSL" base64
+   * 
+   * 3. Recoger el identificador de la aplicación (en facebook developers, dentro de la app, en la zona superior izquierda)
+   * 4. MODIFICAR APP ID EN EL ARCHIVO android > app > src > main > res > values > string.xml
+   
+        <string name="facebook_app_id">748077676570420</string>
+        <string name="fb_login_protocol_scheme">fb748077676570420</string>
+        <string name="facebook_client_token">748077676570420</string>
+
+   * 5. Dentro del developers : Revisión de la aplicación > Permisos y funciones
+   *      - Dar permiso a public_profile
+   *      - Dar permiso a email
+   *      - Realizar la comprobación de uso de datos (tendremos una alerta en naranja).
+   *    * 
+   * ===================================================================================
+   */
 
   async loginFacebook() {
     const result = await FacebookLogin.login({ permissions: FACEBOOK_PERMISSIONS });
@@ -127,6 +162,12 @@ export class LoginPage implements OnInit {
 
     console.log("url " , url);
     
+  }
+
+
+  
+  public loginApple(){
+
   }
 
 }
