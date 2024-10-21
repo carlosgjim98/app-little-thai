f93db63b22d82c701bcdaf104eeff90cf9648e05
Sergio <sergiogallego@xerintel.es>
2024-10-21 14:13:02 +0200

    correcciones

--- src/app/pages/forgot-password/forgot-password.module.ts
+++ src/app/pages/forgot-password/forgot-password.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -16,6 +17,7 @@ import { TranslateModule } from '@ngx-translate/core';
     IonicModule,
     ForgotPasswordPageRoutingModule,
     TranslateModule.forChild(),
+    ComponentsModule
   ],
   declarations: [ForgotPasswordPage]
 })
