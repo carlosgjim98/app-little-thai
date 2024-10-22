c43506ae89bfbd87b05d62f6ea6d94acee6df37f
Sergio <sergiogallego@xerintel.es>
2024-10-22 10:38:42 +0200

    cambios arsbinari

--- src/app/pages/confirm-address/confirm-address.module.ts
+++ src/app/pages/confirm-address/confirm-address.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,8 @@ import { ConfirmAddressPage } from './confirm-address.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ConfirmAddressPageRoutingModule
+    ConfirmAddressPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ConfirmAddressPage]
 })
