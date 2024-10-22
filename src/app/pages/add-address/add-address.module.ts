69a0004103d3943cb0989ab9db73c686bfb2965c
Sergio <sergiogallego@xerintel.es>
2024-10-22 09:25:45 +0200

    cambios address

--- src/app/pages/add-address/add-address.module.ts
+++ src/app/pages/add-address/add-address.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,8 @@ import { AddAddressPage } from './add-address.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    AddAddressPageRoutingModule
+    AddAddressPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [AddAddressPage]
 })
