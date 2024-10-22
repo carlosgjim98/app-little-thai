563badabbbb7a2cd121ec70e8294d2dae4b7d7f0
Sergio <sergiogallego@xerintel.es>
2024-10-22 08:09:38 +0200

    cambios

--- src/app/pages/verificacion/verificacion.module.ts
+++ src/app/pages/verificacion/verificacion.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,8 @@ import { VerificacionPage } from './verificacion.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    VerificacionPageRoutingModule
+    VerificacionPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [VerificacionPage]
 })
