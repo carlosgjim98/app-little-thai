bdb40f34563d911a612a6d5fd188d1f1cd11c6f7
carlosgonzalez3 <carlosgonzalez@xerintel.es>
2024-10-24 14:23:32 +0200

    cambios

--- src/app/pages/checkout/checkout.page.ts
+++ src/app/pages/checkout/checkout.page.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.page.html',
   styleUrls: ['./checkout.page.scss'],
 })
 export class CheckoutPage implements OnInit {
-
+  payCardChecked: boolean = false;
+  payDatChecked: boolean = false;
+  payCashChecked: boolean = false;
   constructor() { }
 
   ngOnInit() {
